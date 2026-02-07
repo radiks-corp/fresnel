5358b8740953b32a5c5a478d8e942b892b95f586
Mitchell Hynes <mitch@spellbook.legal>
2026-02-06 22:12:49 -0330

    Relax CORS for easier local testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -53,7 +53,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
 // Middleware
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: '*',
   credentials: true
 }))
 app.use(express.json())
